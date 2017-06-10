eb4d891a57734022ccc7b711ec219e650fabd244
Caroline Chen <carolinecaroline@gmail.com>
2017-06-09 22:14:42 -0400

    changed port to 8080

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const models = require("./db/schema");
 const path = require("path");
 
+app.set("port", process.env.PORT || 8080)
 app.use(bodyParser.json({extended: true}));
 app.use("/assets", express.static("public"));
 
@@ -168,6 +169,6 @@ app.get("/*", (req, res) => {
 });
 
 
-app.listen(4000, () => {
-  console.log("app listening on port 4000");
+app.listen(app.get("port"), () => {
+  console.log("deploy me");
 });
